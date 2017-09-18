b56eaffe4a3b070b34ac43ac77c00f45a5d7aadb
Xin Bian <xinbian@users.noreply.github.com>
2017-09-18 19:41:53 -0400

    Update RT_forceSymm.py
    add comments

--- RT_forceSymm/RT_forceSymm.py
+++ RT_forceSymm/RT_forceSymm.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-#you can use this to extend RT domain in z direction
-#orginal nx ny nz after nx ny 2*nz
-#velocity is set to zero 
-#density is set to horizotal average at top and bottom with 40 grid porints offset
-#pressure is similar to density and use hydrostatic equation rho*g*dz
+#you can use this to impose symmetry on output hdf5 file
+#you need to specify the timestep
 @author: Xin
 """
 
